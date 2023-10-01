215a0ea3b33bc8552c01c995e5f5fe4f39d52f58
David Lister <cwsfdavidl@gmail.com>
2023-10-01 11:31:07 -0700

    Starting to flesh out model definition

---
+++ analysis_tools.py
@@ -0,0 +1,8 @@
+# AnalysisTools.py
+#
+# Main meta module for the AnalysisTools package.
+# David Lister
+# October 2023
+#
+
+
---
+++ common.py
@@ -0,0 +1,11 @@
+# common.py
+#
+# A file to hold shared symbolic constants.
+# David Lister
+# October 2023
+#
+
+FIXED_PARAMETERS = "FIXED_PARAMETERS"
+FIT_PARAMETERS = "FIT_PARAMETERS"
+MODEL = "MODEL"
+AUTO = "AUTO"  # For automatically determining initial guess
